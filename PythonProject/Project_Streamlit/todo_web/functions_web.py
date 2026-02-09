5d6f3e2cf2c2460a464a28e6e668365f05d9eb0d
michaelab <122539675+Michael-bit-pixel@users.noreply.github.com>
2026-02-09 13:04:02 +0000

    Finallyy

--- PythonProject/Project_Streamlit/todo_web/functions_web.py
+++ PythonProject/Project_Streamlit/todo_web/functions_web.py
@@ -9,9 +9,9 @@ def get_todos(filepath="PythonProject/Project_Streamlit/todo_web/todos_web.txt")
     return todos_local
 
 
-def write_todos(todos_arg,filepath="todos_web.txt"):
+def write_todos(todos_arg,filepath="PythonProject/Project_Streamlit/todo_web/todos_web.txt"):
     """ Write a list of to-do items to a text file."""
-    with open(filepath, "w") as file:
+    with open(os.path.join(os.getcwd(),filepath), "w") as file:
         file.writelines(todos_arg)
 
 if __name__ == "__main__":
