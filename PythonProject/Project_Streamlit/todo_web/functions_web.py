81f9d51cd6b80a469b2c7158370ce886d40c614a
michaelab <122539675+Michael-bit-pixel@users.noreply.github.com>
2026-02-09 13:03:07 +0000

    And Again

--- PythonProject/Project_Streamlit/todo_web/functions_web.py
+++ PythonProject/Project_Streamlit/todo_web/functions_web.py
@@ -1,6 +1,6 @@
 import os
 
-def get_todos(filepath="todo_web/todos_web.txt"):
+def get_todos(filepath="PythonProject/Project_Streamlit/todo_web/todos_web.txt"):
     """ Read a text file and return the list of
     to-do items.
     """
